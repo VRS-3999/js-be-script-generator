9c2095ce423fedc8009f0ed599cb3c25317db38f
vivek <vivek>
2026-02-07 20:10:51 +0530

    inline query added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Dict, Any
-from settings.config import BG_SQL_EXECUTOR, BT_TO_BQ_STREAMING
+from settings.config import BG_SQL_EXECUTOR, BT_TO_BQ_STREAMING, INLINE_SQL, EXTERNAL_SQL, DAG_REPO
 from gemini.core import DagGenerator, CFG
 from utils.common import _normalize_list
 
@@ -33,26 +33,28 @@ async def generate_script(payload: Dict[str, Any]):
             cfg=CFG,
             system_instruction_md_path="prompts/system_instruction.md",
         )
-        with open("templates/bg_sql_executor_file_input.py.template", "r") as f:
-            dag_template = f.read()
-        external_sql_filename = (
-            payload.get("external_sql_file", {})
-            .get("fileList", [{}])[0]
-            .get("name")
-        )
-        if not external_sql_filename:
-            raise ValueError("external_sql_file.fileList[0].name is required")
         dag_config = {
+            "app": payload.get("app", ""),
+            "bq_streaming_table_id": payload.get("bq_streaming_table_id", ''),
+            "bq_table_id": payload.get("bq_table_id", ''),
+            "bq_tenant": payload.get("bq_tenant", ''),
+            "brief_description": payload.get("brief_description", ''), # NOT USED IN TEMPLATES
+            "cost_center": payload.get("cost_center", ""),
+            "dag_id": payload.get("dag_id", ""),
+            "to_emails": _normalize_list(payload.get("dev_failure_emails", '')),
+            "dev_success_emails": _normalize_list(payload.get("dev_success_emails", '')), # NOT USED IN TEMPLATES
+            "prod_failure_emails": _normalize_list(payload.get("prod_failure_emails", '')), # NOT USED IN TEMPLATES
+            "prod_success_emails": _normalize_list(payload.get("prod_success_emails", '')), # NOT USED IN TEMPLATES
+            "test_failure_emails": _normalize_list(payload.get("test_failure_emails", '')), # NOT USED IN TEMPLATES
+            "test_success_emails": _normalize_list(payload.get("test_success_emails", '')), # NOT USED IN TEMPLATES
+            "owner_email": payload.get("email", ''),
+            "lob": payload.get("lob", ""),
+            "schedule_interval": payload.get("schedule_interval", "0 * * * *"),
+            "tenant": payload.get("tenant", ""),
             "project_id": CFG.project_id,
             "region": CFG.region,
-            "tenant": payload.get("tenant", ""),
-            "app": payload.get("app", ""),
-            "lob": payload.get("lob", ""),
             "username": payload.get("username", ""),
-            "cost_center": payload.get("cost_center", ""),
-
-            "dag_repo": "auto-pa-features-update-bq",
-            "dag_id": payload.get("dag_id", ""),
+            "dag_repo": DAG_REPO,
             "dag_tags": [
                 f"tenant:{payload.get('tenant', '')}",
                 f"app:{payload.get('app', '')}",
@@ -60,25 +62,35 @@ async def generate_script(payload: Dict[str, Any]):
                 f"cost-center:{payload.get('cost_center', '')}",
             ],
 
-            "bq_tenant": payload.get("bq_tenant", ''),
-            "bq_table_id": payload.get("bq_table_id", ''),
-            "bq_streaming_table_id": payload.get("bq_streaming_table_id", ''),
-
-            "owner_email": payload.get("email", ''),
-            "to_emails": _normalize_list(payload.get("dev_failure_emails", '')),
             "cc_emails": [],
-
             "notify_success": payload.get("notify_success", ''),
             "notify_failure": payload.get("notify_failure", ''),
-            "schedule_interval": payload.get("schedule_interval", "0 * * * *"),
-            "sql_file_name": external_sql_filename,
         }
-
-        result = generator.generate_and_write_dag(
-            dag_template=dag_template,
-            payload=dag_config,
-        )
-        return result
+        if str(payload.get('sql_source_type')).upper() == EXTERNAL_SQL:
+            with open("templates/bg_sql_executor_file_input.py.template", "r") as f:
+                dag_template = f.read()
+            external_sql_filename = (
+                payload.get("external_sql_file", {})
+                .get("fileList", [{}])[0]
+                .get("name")
+            )
+            if not external_sql_filename:
+                raise ValueError("external_sql_file.fileList[0].name is required")
+            dag_config.update("sql_file_name", external_sql_filename)
+            result = generator.generate_and_write_dag(
+                dag_template=dag_template,
+                payload=dag_config,
+            )
+            return result
+        elif str(payload.get('sql_source_type')).upper() == INLINE_SQL:
+            with open("templates/bg_sql_executor_inline_sql.template", "r") as f:
+                dag_template = f.read()
+            dag_config.update("inline_sql_query", payload.get("inline_sql_query", ""))
+            result = generator.generate_and_write_dag(
+                dag_template=dag_template,
+                payload=dag_config,
+            )
+            return result
     elif str(payload.get("dag_type")).upper() == BT_TO_BQ_STREAMING:
         generator = DagGenerator(
             cfg=CFG,
